4de18ed418e8bff5e134e219c5a1e92213594499
scribencode <deyeye99@gmail.com>
2026-02-22 21:18:58 +0100

    add website structure and folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// import {Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container'
+// import {CTA, Brand, Navbar} from './components'
+const App = () => {
+  return (
+    <div>
+        <h1>GPT-3</h1>
+        <h2>Adeyeye</h2>
+    </div>
+  )
+}
+
+export default App
